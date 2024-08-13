9e685ebfaeb07bb68a84d4dd2faec0240ff8f448
Hugo Vazquez <tomynotemueras@hotmail.com>
2024-08-13 12:26:25 +0200

    Agregar archivos de proyecto.

---
+++ Triggers/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿namespace Triggers
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public int Count 
+        { 
+            get => count;
+            set
+            {
+                count = value;
+                OnPropertyChanged(nameof(Count));
+            }
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+        }
+
+        
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            Count++;
+
+            if (Count == 1)
+            {
+                CounterBtn.Text = $"Clicked {Count} time";
+
+                MultiTriggerButton.Text = $"Clicked {Count} time";
+            }
+            else
+            {
+                CounterBtn.Text = $"Clicked {Count} times";
+
+                MultiTriggerButton.Text = $"Clicked {Count} times";
+            }
+                
+            SemanticScreenReader.Announce(CounterBtn.Text);
+
+            SemanticScreenReader.Announce(MultiTriggerButton.Text);
+        }
+    }
+
+}
---
+++ Triggers/NumericValidationTriggerAction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triggers
+{
+    public class NumericValidationTriggerAction : TriggerAction<Entry>
+    {
+        public int MinValue { get; set; }
+        protected override void Invoke(Entry sender)
+        {           
+            if (sender.Text == "valid")
+            {
+                sender.BackgroundColor = Colors.Green;
+            }
+            else
+            {
+                sender.BackgroundColor = Colors.White;
+            }
+
+        }
+    }
+}
